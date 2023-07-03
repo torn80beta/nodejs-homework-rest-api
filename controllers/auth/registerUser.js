0177247c0ee41d4fd47b78335f6490c8c01d0b09
Alex <103459735+torn80beta@users.noreply.github.com>
2023-07-03 16:52:11 +0300

    bcrypt

--- controllers/auth/registerUser.js
+++ controllers/auth/registerUser.js
@@ -1,14 +1,19 @@
 const { User } = require('../../models/user');
 const { httpError } = require('../../helpers');
+const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (user) {
     throw httpError(409, 'Email already in use');
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+
   res.status(201).json({
     email: newUser.email,
     name: newUser.name,
