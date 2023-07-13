330c1099dd84e91fc01ac79b51d87efda92feeca
Alex <103459735+torn80beta@users.noreply.github.com>
2023-07-13 19:48:44 +0300

    send greed integration

--- controllers/auth/loginUser.js
+++ controllers/auth/loginUser.js
@@ -27,6 +27,27 @@ const loginUser = async (req, res) => {
 
   await User.findByIdAndUpdate(user._id, { token });
 
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+
+  // const sgMail = require('@sendgrid/mail');
+  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // const msg = {
+  //   to: 'hotbox80@gmail.com',
+  //   from: 'auth_service@meta.ua',
+  //   subject: 'Sending with SendGrid is Fun',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // };
+  // sgMail
+  //   .send(msg)
+  //   .then(() => {
+  //     console.log('Email sent');
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+
   res.json({
     token,
     user: { email: user.email, subscription: user.subscription },
